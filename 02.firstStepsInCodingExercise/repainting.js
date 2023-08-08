a8ea0ad5958471e4f1952460c11aca084acd1183
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-08-08 19:24:14 +0300

    repainting

---
+++ 02.firstStepsInCodingExercise/repainting.js
@@ -0,0 +1,20 @@
+function repainting(input){
+let naylonQuantity=Number(input[0]);
+let paintQuantity=Number(input[1]);
+let thinnerQuantity=Number(input[2]);
+let workTime=Number(input[3]);
+let additionalPainQuantity=0.10*paintQuantity;
+let totalPaintQuantity=paintQuantity+additionalPainQuantity;
+let totalNaylonQuantity=naylonQuantity+2;
+let naylonPrice=totalNaylonQuantity*1.50;
+let paintPrice=totalPaintQuantity*14.50;
+let thinnerPrice=thinnerQuantity*5.00;
+let totalMaterialPrice=naylonPrice+paintPrice+thinnerPrice+0.40;
+let workPricePerHour=0.30*totalMaterialPrice;
+let totalWorkPrice=workTime*workPricePerHour;
+let totalPrice=totalMaterialPrice+totalWorkPrice;
+console.log(totalPrice);
+}
+
+
+repainting(["10","11","4","8"])
