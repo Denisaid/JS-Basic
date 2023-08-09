a6d4c70f8433ad3fe574546b5b65c49aa96cb5e9
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-08-09 14:32:00 +0300

    exceent

---
+++ 03.conditionalStatementsLab/exceent.js
@@ -0,0 +1,10 @@
+function result(input) {
+    let grade = Number(input[0]);
+    if (grade >= 5.50) {
+        console.log("Excellent!");
+    } else{
+        console.log("Not excellent");
+    }
+}
+
+result("5")
