63d0deb1d68b965ea621f20b4c757742acece278
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-08-12 20:20:46 +0300

    bonusScore

---
+++ 04.Conditional Statements - Exercise/bonusScore.js
@@ -0,0 +1,21 @@
+function bonusScore(input) {
+    let point = Number(input[0]);
+    let bonus = 0;
+    if (point <= 100) {
+        bonus = 5;
+
+    } else if (point <= 1000) {
+        bonus = point * 0.20;
+
+    } else {
+        bonus = point * 0.10;
+
+    } if (point % 2 === 0) {
+        bonus = bonus + 1;
+    } else if (point % 10 === 5) {
+        bonus = bonus + 2;
+    }
+
+    console.log(bonus);
+    console.log(point+bonus);
+}
