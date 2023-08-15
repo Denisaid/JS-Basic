4a9a8046109ced782589e08f4f869d2648e218f2
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-08-15 19:30:07 +0300

    animalType

---
+++ 05.Conditional Statements Advanced - Lab/animalType.js
@@ -0,0 +1,20 @@
+function animalType(input) {
+  let name = input[0];
+  switch (name) {
+    case "dog":
+      console.log("mammal");
+      break;
+    case "crocodile":
+    case "tortoise":
+    case "snake":
+      console.log("reptile");
+      break;
+
+    default:
+      console.log("unknown");
+      break;
+  }
+}
+
+
+animalType(["snake"])
