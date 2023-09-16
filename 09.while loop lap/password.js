16c4f11732ec0cfe75344b5f95b875a1630fb32c
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-09-16 19:58:46 +0300

    password

---
+++ 09.while loop lap/password.js
@@ -0,0 +1,29 @@
+function password(input){
+let index=0;
+
+let name=input[index];
+index++;
+
+let pass=input[index];
+index++;
+
+let temPasssword=input[index];
+index++;
+
+while(pass!==temPasssword){
+    temPasssword=input[index];
+    index++;
+}
+console.log(`Welcome ${name}!`);
+
+}
+
+password(["Nakov",
+
+"1234",
+
+"Pass",
+
+"1324",
+
+"1234"])
