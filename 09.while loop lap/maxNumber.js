e96f8e9076b7b6e20d07276eec313bbb22066c39
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-09-16 19:58:29 +0300

    maxNumber

---
+++ 09.while loop lap/maxNumber.js
@@ -0,0 +1,23 @@
+function maxNumber(input){
+
+let index=0;    
+let command=input[index];
+index++;
+
+let myMaxNumber=Number.MIN_SAFE_INTEGER;
+
+while(command!=='Stop'){
+    let num=Number(command);
+    
+if (myMaxNumber<num){
+    myMaxNumber=num;
+}
+
+    command=input[index];
+    index++;
+}
+console.log(myMaxNumber);
+}
+
+
+maxNumber(["45", "-20", "7", "99", "Stop"])
