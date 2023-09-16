a6e1e1e38e0b412631713bf5ddab4a64615b2cd5
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-09-16 19:58:37 +0300

    minNumbers

---
+++ 09.while loop lap/minNumbers.js
@@ -0,0 +1,31 @@
+function maxNumber(input){
+
+    let index=0;    
+    let command=input[index];
+    index++;
+    
+    let myMinNumber=Number.MAX_SAFE_INTEGER;
+    
+    while(command!=='Stop'){
+        let num=Number(command);
+        
+    if (myMinNumber>num){
+        myMinNumber=num;
+    }
+    
+        command=input[index];
+        index++;
+    }
+    console.log(myMinNumber);
+    }
+    
+    
+    maxNumber(["100",
+    
+    "99",
+    
+    "80",
+    
+    "70",
+    
+    "Stop"])
