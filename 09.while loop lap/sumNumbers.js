efcc91ece30d2d31020fa25977f9a944954f9471
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-09-16 19:59:10 +0300

    sumNumbers

---
+++ 09.while loop lap/sumNumbers.js
@@ -0,0 +1,16 @@
+function sumNumbers(input){
+let n=Number(input[0]);
+let sum=0;
+let index=1;
+
+while(sum<n){
+    let currentNum=Number(input[index]);
+    sum+=currentNum;
+    index++;
+}
+console.log(sum);
+
+}
+
+
+sumNumbers(["20", "1", "2", "3", "4", "5", "6"])
