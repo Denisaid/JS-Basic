d0bbfc279fea8fe75a47d7c69b1d956791498a3e
Deniz Said <138148614+Denisaid@users.noreply.github.com>
2023-09-21 20:36:06 +0300

    01.clock

---
+++ 11.Nested Loops lab/01.clock.js
@@ -0,0 +1,22 @@
+function clock(input){
+for (let hours=0; hours<24; hours++){
+    for (let min=0; min<60; min++){
+        let buff='';
+        if (hours<10){
+            buff+='0'+hours;
+        } else {
+            buff+=hours;
+        }
+        buff+=':';
+        if (min<10){
+            buff+='0'+min;
+        } else {
+            buff+=min;
+        }
+        console.log(buff);
+    }
+} 
+
+}
+
+clock()
